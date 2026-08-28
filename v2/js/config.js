7409521fb6f7374ff9c6832c2ba6072176e24976
Macarenagalvan <79223918+Macarenagalvan@users.noreply.github.com>
2026-08-28 13:48:56 +0200

    v2: RGM_SOURCE_CONTEXT = LIVE

--- v2/js/config.js
+++ v2/js/config.js
@@ -16,6 +16,7 @@ export const ROADMAP_ASSETS = [
 export const SESSIONS = ["SYDNEY", "TOKYO", "LONDON", "NEW_YORK"];
 
 export const RGM_SOURCE_ASSET = "SP500";
+export const RGM_SOURCE_CONTEXT = "LIVE";
 
 export const OBSERVATION_TAG_SUGGESTIONS = [
   "false-break",
